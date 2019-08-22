8ee1e75f598588703d0fff678048984183b06f88
zhousg <zhoushugang@itcast.cn>
2019-08-22 18:00:04 +0800

    配置登录的路由和组件

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    App <el-button type="success">成功按钮</el-button>
+    <!-- 定义一个 一级路由的出口  组件渲染的位置 -->
+    <router-view></router-view>
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -2,10 +2,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  // 定义路由规则（路径==>组件）
+  routes: [
+    // name选项作用  找到对应的路由规则
+    // 跳转方便一些：$router.push('/login') 或者 $router.push({name:'login'})
+    { path: '/login', name: 'login', component: Login }
+  ]
 })
 
 export default router
