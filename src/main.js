03c528a7e89b1ad17d3a80a934e815eb8d3a18d0
zhousg <zhoushugang@itcast.cn>
2019-08-22 21:55:14 +0800

    绘制登录页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import '@/styles/index.less'
+
 // @ 是vue-cli提供的src绝对路径别名
 // index.js 省略了  在目录下默认索引文件（默认会去加载文件index名称）
 // index.js index.vue index.json 优先级和书写顺序一致，默认三种文件
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,61 @@
 <template>
-  <div class='container'>Login</div>
+  <div class='container'>
+    <!-- 卡片 element-ui 组件 -->
+    <el-card class="my-card">
+      <img src="../../assets/images/logo_index.png" alt="">
+      <el-form :model="loginForm">
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:236px;margin-right:10px"></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+           <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: ''
+      }
+    }
+  }
+}
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.container{
+  background: url(../../assets/images/login_bg.jpg) no-repeat center / cover;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-card{
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+    img{
+      width: 200px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+  }
+}
+
+</style>
