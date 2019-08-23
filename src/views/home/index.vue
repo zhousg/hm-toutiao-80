7833767faf95c664d8226cd3159283592aab86df
zhousg <zhoushugang@itcast.cn>
2019-08-23 18:23:32 +0800

    导航切换效果&开启路由功能

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,41 +1,45 @@
 <template>
   <el-container class="my-container">
-    <el-aside width="200px">
+    <el-aside :width="isCollapse?'64px':'200px'">
       <!-- logo -->
-      <div class="logo"></div>
+      <div class="logo" :class="{miniLogo:isCollapse}"></div>
       <!-- 导航菜单 -->
       <el-menu
-        default-active="1"
+        default-active="/"
         class="el-menu-vertical-demo"
         background-color="#002033"
         text-color="#fff"
         active-text-color="#ffd04b"
+        :collapse="isCollapse"
+        :collapse-transition="false"
+        router
       >
-        <el-menu-item index="1">
+        <!-- 属性的时候  如果是布尔类型的属性  有属性代码true  没有属性代表false -->
+        <el-menu-item index="/">
           <i class="el-icon-s-home"></i>
           <span slot="title">首页</span>
         </el-menu-item>
-        <el-menu-item index="2">
+        <el-menu-item index="/article">
           <i class="el-icon-document"></i>
           <span slot="title">内容管理</span>
         </el-menu-item>
-        <el-menu-item index="3">
+        <el-menu-item index="/image">
           <i class="el-icon-picture"></i>
           <span slot="title">素材管理</span>
         </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item index="/publish">
           <i class="el-icon-s-promotion"></i>
           <span slot="title">发布文章</span>
         </el-menu-item>
-        <el-menu-item index="5">
+        <el-menu-item index="/comment">
           <i class="el-icon-chat-dot-round"></i>
           <span slot="title">评论管理</span>
         </el-menu-item>
-        <el-menu-item index="6">
+        <el-menu-item index="/fans">
           <i class="el-icon-present"></i>
           <span slot="title">粉丝管理</span>
         </el-menu-item>
-        <el-menu-item index="7">
+        <el-menu-item index="/setting">
           <i class="el-icon-setting"></i>
           <span slot="title">个人设置</span>
         </el-menu-item>
@@ -43,7 +47,7 @@
     </el-aside>
     <el-container>
       <el-header>
-        <span class="icon el-icon-s-fold"></span>
+        <span @click="toggleAside()" class="icon el-icon-s-fold"></span>
         <span class="text">江苏传智播客科技教育有限公司</span>
         <!-- 下拉菜单组件 -->
         <el-dropdown class="my-dropdown">
@@ -68,7 +72,20 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      isCollapse: false
+    }
+  },
+  methods: {
+    toggleAside () {
+      // 切换两个状态  展开  收起
+      // vue 操作样式  :style  :class
+      this.isCollapse = !this.isCollapse
+    }
+  }
+}
 </script>
 
 <style scoped lang='less'>
@@ -88,6 +105,11 @@ export default {}
       height: 60px;
       background: #002244 url(../../assets/images/logo_admin.png) no-repeat
         center / 140px auto;
+    }
+    // logo 后面 覆盖之前的样式
+    .miniLogo {
+      background-image: url(../../assets/images/logo_admin_01.png);
+      background-size: 36px auto;
     }
     .el-menu{
       border-right: none;
