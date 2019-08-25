cfe23a3b39d59b366e8e92bf8753a486bfd22f1c
zhousg <zhoushugang@itcast.cn>
2019-08-25 21:42:42 +0800

    测试

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Login from '@/views/login'
 import Home from '@/views/home'
 import Welcome from '@/views/welcome'
 import NotFound from '@/views/404'
+import Test from '@/views/test'
 
 Vue.use(VueRouter)
 
@@ -16,6 +17,7 @@ const router = new VueRouter({
     // name选项作用  找到对应的路由规则
     // 跳转方便一些：$router.push('/login') 或者 $router.push({name:'login'})
     { path: '/login', name: 'login', component: Login },
+    { path: '/test', name: 'test', component: Test },
     {
       path: '/',
       component: Home,
---
+++ src/views/test/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class='container'>Test</div>
+</template>
+
+<script>
+export default {
+  async created () {
+    // 1. 使用promise 调用A接口后调用B接口获取所有的数据
+    // this.$http.get('http://localhost:3000/a')
+    //   .then(res => {
+    //     console.log(res.data) // A
+    //     return this.$http.get('http://localhost:3000/b')
+    //   })
+    //   .then(res => {
+    //     console.log(res.data) // B
+    //   })
+
+    // 2. 使用async await 调用A接口后调用B接口获取所有的数据
+    // 使用 await 修饰的函数（返回值promise） 那么：返回值成功后的结果
+    // await 同步执行这个函数，阻碍当前程序执行
+    // await 的使用必须在 async 修饰的函数内部 （await的外层函数必须加上async ）
+    const resA = await this.$http.get('http://localhost:3000/a')
+    const resB = await this.$http.get('http://localhost:3000/b')
+    console.log(resA.data, resB.data)
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
