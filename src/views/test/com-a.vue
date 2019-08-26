817ca436a173ccd23b3bc8e7c731d7bd04f2e3fd
zhousg <zhoushugang@itcast.cn>
2019-08-26 18:02:02 +0800

    插槽

---
+++ src/views/test/com-a.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='container'>
+    <!-- 组件内部 -->
+    <div>header</div>
+    <div><slot name="content" :msg="message" list="数组">默认内容</slot></div>
+    <div><slot name="footer">底部内容</slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      message: '组件内部数据com-a'
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'></style>
--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,28 +1,42 @@
 <template>
-  <div class='container'>Test</div>
+  <div class='container'>
+    <!-- 使用组件 -->
+    <com-a>
+      <!-- slot="content" 指定插槽名称  -->
+      <!-- slot-scope="scope" 接收作用域插槽上所有绑定的数据，数据的集合名字叫scope对象{msg:'',list:''} -->
+      <!-- <div slot="content" slot-scope="scope">内容1 {{scope.msg}}</div> -->
+      <!-- 在vue版本 2.6.0 之后提供了新的写法  v-slot:插槽名称="作用域数据对象" -->
+      <template v-slot:content="scope">内容1 {{scope.msg}}</template>
+      <div slot="footer">底部1</div>
+    </com-a>
+    <hr>
+    <com-a></com-a>
+  </div>
 </template>
 
 <script>
+import ComA from './com-a'
 export default {
-  async created () {
-    // 1. 使用promise 调用A接口后调用B接口获取所有的数据
-    // this.$http.get('http://localhost:3000/a')
-    //   .then(res => {
-    //     console.log(res.data) // A
-    //     return this.$http.get('http://localhost:3000/b')
-    //   })
-    //   .then(res => {
-    //     console.log(res.data) // B
-    //   })
+  components: { ComA }
+  // async created () {
+  //   // 1. 使用promise 调用A接口后调用B接口获取所有的数据
+  //   // this.$http.get('http://localhost:3000/a')
+  //   //   .then(res => {
+  //   //     console.log(res.data) // A
+  //   //     return this.$http.get('http://localhost:3000/b')
+  //   //   })
+  //   //   .then(res => {
+  //   //     console.log(res.data) // B
+  //   //   })
 
-    // 2. 使用async await 调用A接口后调用B接口获取所有的数据
-    // 使用 await 修饰的函数（返回值promise） 那么：返回值成功后的结果
-    // await 同步执行这个函数，阻碍当前程序执行
-    // await 的使用必须在 async 修饰的函数内部 （await的外层函数必须加上async ）
-    const resA = await this.$http.get('http://localhost:3000/a')
-    const resB = await this.$http.get('http://localhost:3000/b')
-    console.log(resA.data, resB.data)
-  }
+  //   // 2. 使用async await 调用A接口后调用B接口获取所有的数据
+  //   // 使用 await 修饰的函数（返回值promise） 那么：返回值成功后的结果
+  //   // await 同步执行这个函数，阻碍当前程序执行
+  //   // await 的使用必须在 async 修饰的函数内部 （await的外层函数必须加上async ）
+  //   const resA = await this.$http.get('http://localhost:3000/a')
+  //   const resB = await this.$http.get('http://localhost:3000/b')
+  //   console.log(resA.data, resB.data)
+  // }
 }
 </script>
 
